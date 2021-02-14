8a4b0e015517c6c904c5f9e86ec2e43b427fd080
Miguel Reyes <mreyes@life360.com>
2021-02-13 22:58:55 -0800

    Mux router welcome

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(w, "<h1>Welcome!</h1>")
+}
 
 func main() {
-	fmt.Println("Hello World")
+	mux := mux.NewRouter().StrictSlash(true)
+	mux.HandleFunc("/", home)
+	fmt.Println("Listening on http://localhost:8080/")
+	http.ListenAndServe(":8080", mux)
 }
