65b7ba7c9ef32b528c6ec0e2c9ab148fe6d5271f
Miguel Reyes <mreyes@life360.com>
2021-05-28 16:20:12 -0700

    More prometheus metrics

--- README.md
+++ README.md
@@ -14,5 +14,18 @@ Note: Redis will cache the value even if the container is stopped.
 
 # URLs
 
+* App: http://localhost:9000
 * Prometheus: http://localhost:9090
 * Grafana: http://localhost:3000
+
+# Load tests
+
+```
+hey -z 5m -q 5 -m GET -H "Accept: text/html" http://127.0.0.1:9000
+```
+
+Graphana dashboard: https://gist.github.com/netrebel/b64c4af742c11d61a0fdf0763979515a
+
+# Refs:
+
+* Monitoring implementation. See https://gabrieltanner.org/blog/collecting-prometheus-metrics-in-golang
--- promutil/prom.go
+++ promutil/prom.go
@@ -3,13 +3,17 @@ package promutil
 import (
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
 func init() {
 	prometheus.Register(totalRequests)
+	prometheus.Register(responseStatus)
+	prometheus.Register(httpDuration)
 }
 
 var totalRequests = prometheus.NewCounterVec(
@@ -20,15 +24,35 @@ var totalRequests = prometheus.NewCounterVec(
 	[]string{"path"},
 )
 
+var responseStatus = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "response_status",
+		Help: "Status of HTTP response",
+	},
+	[]string{"status"},
+)
+
+var httpDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
+	Name: "http_response_time_seconds",
+	Help: "Duration of HTTP requests.",
+}, []string{"path"})
+
 func PrometheusMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		route := mux.CurrentRoute(r)
 		path, _ := route.GetPathTemplate()
+
+		timer := prometheus.NewTimer(httpDuration.WithLabelValues(path))
 		rw := NewResponseWriter(w)
 		next.ServeHTTP(rw, r)
 
 		log.Printf("inc for path: %v\n", path)
 		totalRequests.WithLabelValues(path).Inc()
+
+		responseStatus.WithLabelValues(strconv.Itoa(rw.statusCode)).Inc()
+		totalRequests.WithLabelValues(path).Inc()
+
+		timer.ObserveDuration()
 	})
 }
 
